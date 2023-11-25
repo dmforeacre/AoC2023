57aba8a218b2889c5f269d76636dc4adbae5521d
dmforeacre <dmforeacre@gmail.com>
2023-11-25 16:55:50 -0500

    Added padding to window elements

--- main.py
+++ main.py
@@ -16,16 +16,17 @@ testFiles = []
 root = Tk()
 root.title("Advent of Code 2023")
 frm = ttk.Frame(root, padding=10)
-frm.grid()
+frm.grid(padx=10,pady=10)
 ttk.Style
 numRows = 0
 for x in srcPath.iterdir():
     testFiles.append(x)
     testFunc = partial(RunTest, x)
-    ttk.Label(frm, text="Run "+x.name).grid(column=0, row=numRows)
-    ttk.Button(frm, text="Run Test", command= testFunc).grid(column=1, row=numRows)
+    ttk.Label(frm, text="Run "+x.name[:-3], padding=5).grid(column=0, row=numRows)
+    ttk.Label(frm, text="   ", padding=5).grid(column=1, row=numRows)
+    ttk.Button(frm, text="Run Test", command= testFunc, padding=5).grid(column=2, row=numRows)
     numRows += 1
 allTestsFunc = partial(RunAllTests, testFiles)
-ttk.Button(frm, text="All Tests", command= allTestsFunc).grid(column=1, row=numRows)
-ttk.Button(frm, text="Quit", command=root.destroy).grid(column=0, row=numRows)
+ttk.Button(frm, text="All Tests", command= allTestsFunc, padding=5).grid(column=2, row=numRows)
+ttk.Button(frm, text="Quit", command=root.destroy, padding=5).grid(column=0, row=numRows)
 root.mainloop()

f50beca68553849783021a45fe76e745fece77b7
dmforeacre <dmforeacre@gmail.com>
2023-11-25 16:37:29 -0500

    Test running functionality added

--- main.py
+++ main.py
@@ -1,17 +1,31 @@
 import os
+from pathlib import Path
+from functools import partial
 from tkinter import *
 from tkinter import ttk
 
-def RunTests():
-    print("Tests")
+def RunTest(testPath):
+    os.system("python3 src\\" + testPath.name)
 
-os.system("python3 src\\1_1.py")
+def RunAllTests(testFiles):
+    for f in testFiles:
+        RunTest(f)
+
+srcPath = Path("src/")
+testFiles = []
 root = Tk()
 root.title("Advent of Code 2023")
 frm = ttk.Frame(root, padding=10)
 frm.grid()
 ttk.Style
-ttk.Label(frm, text="Hello World!").grid(column=0, row=0)
-ttk.Button(frm, text="All Tests", command=RunTests).grid(column=1, row=1)
-ttk.Button(frm, text="Quit", command=root.destroy).grid(column=0, row=1)
+numRows = 0
+for x in srcPath.iterdir():
+    testFiles.append(x)
+    testFunc = partial(RunTest, x)
+    ttk.Label(frm, text="Run "+x.name).grid(column=0, row=numRows)
+    ttk.Button(frm, text="Run Test", command= testFunc).grid(column=1, row=numRows)
+    numRows += 1
+allTestsFunc = partial(RunAllTests, testFiles)
+ttk.Button(frm, text="All Tests", command= allTestsFunc).grid(column=1, row=numRows)
+ttk.Button(frm, text="Quit", command=root.destroy).grid(column=0, row=numRows)
 root.mainloop()

80a86beed279e9fddad11ee67b8d23152152e3ed
dmforeacre <dmforeacre@gmail.com>
2023-12-02 12:49:44 -0500

    Added day 2 solutions

---
+++ src/2_1.py
@@ -0,0 +1,25 @@
+maxR = 12
+maxG = 13
+maxB = 14
+sum = 0
+with open("src\\2_1.txt") as f:
+    for line in f:
+        game, moves = line.split(":")
+        game = list(game.split())[1]
+        isPoss = True
+        for m in list(moves.split(";")):
+            for c in list(m.split(",")):
+                num, color = c.split()
+                match color:
+                    case "red":
+                        if int(num) > maxR:
+                            isPoss = False
+                    case "green":
+                        if int(num) > maxG:
+                            isPoss = False
+                    case "blue":
+                        if int(num) > maxB:
+                            isPoss = False
+        if isPoss:
+            sum += int(game)
+print(sum)
---
+++ src/2_2.py
@@ -0,0 +1,21 @@
+sum = 0
+with open("src\\2_1.txt") as f:
+    for line in f:
+        game, moves = line.split(":")
+        game = list(game.split())[1]
+        r = g = b = 0
+        for m in list(moves.split(";")):
+            for c in list(m.split(",")):
+                num, color = c.split()
+                match color:
+                    case "red":
+                        if int(num) > r:
+                            r = int(num)
+                    case "green":
+                        if int(num) > g:
+                            g = int(num)
+                    case "blue":
+                        if int(num) > b:
+                            b = int(num)
+        sum += r * g * b
+print(sum)

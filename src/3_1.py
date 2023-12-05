194d6a012f050c22efb17b35e1575da925d91674
dmforeacre <dmforeacre@gmail.com>
2023-12-04 22:37:44 -0500

    3_1 Almost working

--- src/3_1.py
+++ src/3_1.py
@@ -1,10 +1,62 @@
+import re
+
+def isSymbol(grid, x, y):
+    if grid[x][y].isnumeric() or grid[x][y] == '.':
+        return False
+    return True
+
+regex = re.compile(r'(\D*)(\d*)')
+nums = []
 grid = []
+sum = 0
 with open("src\\3_1.txt") as f:
     for line in f:
-        row = []
-        for c in line:
-            row.append(c)
-        grid.append(row)
-for j in range(0, len(grid)):
-    for i in range(0, len(grid[0])):
-        print(grid[i][j])
+        rowNums = []
+        match = re.findall(regex, line)
+        for m in match:
+            if m[1] != "":
+                rowNums.append(m[1])
+        grid.append(line)
+        nums.append(rowNums)
+for i in range(0, len(grid)):
+    index = 0
+    for num in nums[i]:
+        isPart = False
+        index = grid[i].find(num, index)
+        if i > 0:    
+            if index - 1 > 0:
+                if not isPart and isSymbol(grid, i - 1, index - 1):
+                    print(num, "is a valid part")
+                    isPart = True
+            for j in range(index, index + len(num) + 1):
+                if not isPart and isSymbol(grid, i - 1, j):
+                    print(num, "is a valid part")
+                    isPart = True
+            if index + len(num) + 1 < len(grid[i]):
+                if not isPart and isSymbol(grid, i - 1, index + len(num) + 1):
+                    print(num, "is a valid part")
+                    isPart = True
+        if index - 1 > 0:
+            if not isPart and isSymbol(grid, i, index - 1):
+                print(num, "is valid part")
+                isPart = True
+        if index + len(num) + 1 < len(grid[i]):
+            if not isPart and isSymbol(grid, i, index + len(num)):
+                print(num, "is valid part")
+                isPart = True
+        if i < len(grid) - 1:
+            if index - 1 > 0:
+                if not isPart and isSymbol(grid, i + 1, index - 1):
+                    print(num, "is a valid part")
+                    isPart = True
+            for j in range(index, index + len(num) + 1):
+                if not isPart and isSymbol(grid, i + 1, j):
+                    print(num, "is a valid part")
+                    isPart = True
+            if not isPart and index + len(num) + 1 < len(grid[i]):
+                if isSymbol(grid, i + 1, index + len(num)):
+                    print(num, "is a valid part")
+                    isPart = True
+        if isPart:
+            sum += int(num)
+print(sum)
